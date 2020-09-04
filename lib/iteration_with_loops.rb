4ac41f58da788d277d71eccd7c7a5af3260c2379
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-04 21:16:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,9 +7,15 @@ def join_nested_strings(src)
     while inner_count < src[count].length do
       if src[count][inner_count].class == String
         string_array.push(src[count][inner_count])
+        string_array
+        binding.pry
       end
+      string_array
+      binding.pry
       inner_count += 1
     end
+    string_array
+    binding.pry
     #string_array.join(" ")
     count += 1
   end
